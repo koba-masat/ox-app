a3c892b0747c607ceb10ffd26b31b62f461f2bea
akane <akane.518bmm@gmail.com>
2022-04-12 15:50:44 +0900

    追加

--- ox-app.py
+++ ox-app.py
@@ -21,29 +21,35 @@ thread1 = th.Thread(target=tkButton)
 thread1.start()
 
 with pyvirtualcam.Camera(width=frame.shape[1], height=frame.shape[0], fps=30) as cam:
-    if mark == 1:
-        marubatsu_array = cv2.imread("figure/mark_maru.png")
-    else:
-        marubatsu_array = cv2.imread("figure/mark_batsu.png")
+    maru_array = cv2.imread("figure/mark_maru.png")
+    batsu_array = cv2.imread("figure/mark_batsu.png")
 
-    # marubatsu_array = cv2.imread("figure/mark_maru.png")
-    # batsu_array = cv2.imread("figure/mark_batsu.png")
-    marubatsu_array = cv2.resize(marubatsu_array, dsize=(100, 100))
-    # batsu_array = cv2.resize(batsu_array, dsize=(100, 100))
 
-    # root = tk.Tk()
-    # root.geometry("350x100")
+    if not marubatsu_array:
+        marubatsu_array = cv2.resize(marubatsu_array, dsize=(100, 100))
 
-    # root.mainloop()
     while True:
         # 各フレームの画像を取得
         ret, frame = cap.read()
 
-        x_offset = int((frame.shape[1]-marubatsu_array.shape[1])/2)
-        y_offset = 80
+        if mark == "maru":
+            marubatsu_array = maru_array
+            x_offset = int((frame.shape[1]-marubatsu_array.shape[1])/2)
+            y_offset = 80
 
         frame[y_offset: y_offset + marubatsu_array.shape[0],
               x_offset: x_offset + marubatsu_array.shape[1]] = marubatsu_array
+        elif mark == "batsu":
+            marubatsu_array = batsu_array
+            x_offset = int((frame.shape[1]-marubatsu_array.shape[1])/2)
+            y_offset = 80
+
+        frame[y_offset: y_offset + marubatsu_array.shape[0],
+              x_offset: x_offset + marubatsu_array.shape[1]] = marubatsu_array
+        else:
+            marubatsu_array =
+
+
         frame = np.flip(frame, 2)
         # 画像を仮想カメラに流す
         cam.send(frame)
