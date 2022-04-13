85ffbde4c16a76eaa5599c1cf8e4ace6430dfa6c
shaream <sprinter0208@outlook.jp>
2022-04-13 13:23:52 +0900

    とりあえず関数をクラスに入れた

--- ox-app.py
+++ ox-app.py
@@ -4,6 +4,9 @@ import numpy as np
 import tkinter as tk
 import threading as th
 
+# import ox-tkinter
+from oxTkinter import Controller
+
 # 取得するデータソース（Webカメラ）を選択
 cap = cv2.VideoCapture(0)
 # 最初のフレームから画像のサイズを取得
@@ -14,63 +17,63 @@ x_offset = 100
 y_offset = 100
 
 
-def maru():
-    global mark
-    mark = "maru"
+# def maru():
+#     global mark
+#     mark = "maru"
 
 
-def batsu():
-    global mark
-    mark = "batsu"
+# def batsu():
+#     global mark
+#     mark = "batsu"
 
 
-def nashi():
-    global mark
-    mark = ""
+# def nashi():
+#     global mark
+#     mark = ""
 
 
-def right():
-    global x_offset
-    x_offset += 1
+# def right():
+#     global x_offset
+#     x_offset += 1
 
 
-def left():
-    global x_offset
-    x_offset -= 1
+# def left():
+#     global x_offset
+#     x_offset -= 1
 
 
-def up():
-    global y_offset
-    y_offset -= 1
+# def up():
+#     global y_offset
+#     y_offset -= 1
 
 
-def down():
-    global y_offset
-    y_offset += 1
+# def down():
+#     global y_offset
+#     y_offset += 1
 
 
-def tkButton():
-    root = tk.Tk()
-    root.geometry("300x150")
-    maru_button = tk.Button(root, text="まる", command=maru)
-    batsu_button = tk.Button(root, text="ばつ", command=batsu)
-    nashi_button = tk.Button(root, text="なし", command=nashi)
-    right_button = tk.Button(
-        root, text="right", command=right, repeatdelay=1, repeatinterval=10)
-    left_button = tk.Button(root, text="left", command=left,
-                            repeatdelay=1, repeatinterval=10)
-    up_button = tk.Button(root, text="up", command=up,
-                          repeatdelay=1, repeatinterval=10)
-    down_button = tk.Button(root, text="down", command=down,
-                            repeatdelay=1, repeatinterval=10)
-    maru_button.grid(row=1, column=1, padx=50, pady=5, columnspan=2, rowspan=2)
-    batsu_button.grid(row=4, column=1, padx=50, pady=5, columnspan=2, rowspan=2)
-    nashi_button.grid(row=6, column=1, padx=50, pady=5, columnspan=2, rowspan=2)
-    right_button.grid(row=4, column=10, pady=5, columnspan=2, rowspan=2)
-    left_button.grid(row=4, column=6, pady=5, columnspan=2, rowspan=2)
-    up_button.grid(row=1, column=8, pady=5, columnspan=2, rowspan=2)
-    down_button.grid(row=6, column=8, pady=5, columnspan=2, rowspan=2)
-    root.mainloop()
+# def tkButton():
+#     root = tk.Tk()
+#     root.geometry("300x150")
+#     maru_button = tk.Button(root, text="まる", command=maru)
+#     batsu_button = tk.Button(root, text="ばつ", command=batsu)
+#     nashi_button = tk.Button(root, text="なし", command=nashi)
+#     right_button = tk.Button(
+#         root, text="right", command=right, repeatdelay=1, repeatinterval=10)
+#     left_button = tk.Button(root, text="left", command=left,
+#                             repeatdelay=1, repeatinterval=10)
+#     up_button = tk.Button(root, text="up", command=up,
+#                           repeatdelay=1, repeatinterval=10)
+#     down_button = tk.Button(root, text="down", command=down,
+#                             repeatdelay=1, repeatinterval=10)
+#     maru_button.grid(row=1, column=1, padx=50, pady=5, columnspan=2, rowspan=2)
+#     batsu_button.grid(row=4, column=1, padx=50, pady=5, columnspan=2, rowspan=2)
+#     nashi_button.grid(row=6, column=1, padx=50, pady=5, columnspan=2, rowspan=2)
+#     right_button.grid(row=4, column=10, pady=5, columnspan=2, rowspan=2)
+#     left_button.grid(row=4, column=6, pady=5, columnspan=2, rowspan=2)
+#     up_button.grid(row=1, column=8, pady=5, columnspan=2, rowspan=2)
+#     down_button.grid(row=6, column=8, pady=5, columnspan=2, rowspan=2)
+#     root.mainloop()
 
 
 thread1 = th.Thread(target=tkButton)
